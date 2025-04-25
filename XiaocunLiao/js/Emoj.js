2f73146cd148a6541876769473564493a41f4763
cuntou0906 <1084895390@qq.com>
2025-04-25 15:40:35 +0800

    Site updated: 2025-04-25 15:40:33

--- XiaocunLiao/js/Emoj.js
+++ XiaocunLiao/js/Emoj.js
@@ -58,14 +58,19 @@ document.addEventListener('mousemove', (e) => {
 
 // 鼠标点击波纹特效
 document.addEventListener('click', function(event) {
+    // 获取鼠标点击位置相对于视口的位置
+    const rect = document.body.getBoundingClientRect();
+    const x = event.clientX - rect.left; // 计算相对于页面的X坐标
+    const y = event.clientY - rect.top;  // 计算相对于页面的Y坐标
+
     // 创建多个波纹效果元素
     for (let i = 0; i < 5; i++) { // 生成5个波纹
         const ripple = document.createElement('div');
         ripple.className = 'ripple_mouseClick';
         
         // 设置波纹效果的位置
-        ripple.style.left = `${event.clientX - 15}px`; // 使波纹中心在点击位置
-        ripple.style.top = `${event.clientY - 15}px`; // 使波纹中心在点击位置
+        ripple.style.left = `${x - 15}px`; // 使波纹中心在点击位置
+        ripple.style.top = `${y - 15}px`; // 使波纹中心在点击位置
         
         // 随机化波纹的初始大小
         const size = Math.random() * 10 + 20; // 随机大小范围在20px到30px之间
